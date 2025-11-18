bb9690e8600253a3764437c2b04e63a4cb0cb02a
Carlos Rondón Pérez <166508134+carlosrondon95@users.noreply.github.com>
2025-11-18 20:52:15 +0100

    upp
    pantalla reservas

--- rnaceapp/src/app/app.routes.ts
+++ rnaceapp/src/app/app.routes.ts
@@ -5,16 +5,31 @@ import { authGuard } from './core/auth.guard';
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 
+  // Login
   {
     path: 'login',
-    loadComponent: () => import('./auth/login/login.component').then((m) => m.LoginComponent),
+    loadComponent: () =>
+      import('./auth/login/login.component').then((m) => m.LoginComponent),
   },
 
+  // Dashboard (protegido)
   {
     path: 'dashboard',
     canActivate: [authGuard],
     loadComponent: () =>
-      import('./dashboard/dashboard.component').then((m) => m.DashboardComponent),
+      import('./dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent,
+      ),
+  },
+
+  // Reservar cita (protegido, cliente)
+  {
+    path: 'reservar-cita',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./reservas/reserva-cita.component').then(
+        (m) => m.ReservaCitaComponent,
+      ),
   },
 
   { path: '**', redirectTo: 'login' },
