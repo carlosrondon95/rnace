ea455a7ff8af7fb6f1bac0c487ce68ec2f581773
Carlos Rondón Pérez <carlos.rp1205@gmail.com>
2025-12-22 14:38:31 +0100

    up

--- rnaceapp/src/environments/environment.ts
+++ rnaceapp/src/environments/environment.ts
@@ -2,5 +2,14 @@
 export const environment = {
   production: false,
   supabaseUrl: 'https://bpzdpsmwtsmwrlyxzcsk.supabase.co',
-  supabaseAnonKey: '***REMOVED_SUPABASE_KEY***'
+  supabaseAnonKey: '***REMOVED_SUPABASE_KEY***',
+  firebase: {
+    apiKey: 'AIzaSyBqgXG6XpZG6XpZG6XpZG6XpZG6XpZG6Xp',
+    authDomain: 'rnace-50c31.firebaseapp.com',
+    projectId: 'rnace-50c31',
+    storageBucket: 'rnace-50c31.appspot.com',
+    messagingSenderId: '626137220500',
+    appId: '***REMOVED_APP_ID***'
+  },
+  firebaseVapidKey: '***REMOVED_VAPID_KEY***'
 };
