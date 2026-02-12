1e352e2b8aa668f7c14000bdf7e763232126a2bc
Carlos Rondón Pérez <carlos.rp1205@gmail.com>
2026-02-12 10:50:19 +0100

    up

---
+++ rnaceapp/src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+// src/environments/environment.prod.ts
+export const environment = {
+    production: true,
+    supabaseUrl: 'https://bpzdpsmwtsmwrlyxzcsk.supabase.co',
+    supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwemRwc213dHNtd3JseXh6Y3NrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzNTg0MDIsImV4cCI6MjA3NzkzNDQwMn0.ePJ7dYKZz5OllcKxH--1T6N97zKSQS6DZ9ZJ_mljp88',
+    firebase: {
+        apiKey: 'AIzaSyCzBNKc2TwhtqlgCCljwQMQPkPX6ujha1k',
+        authDomain: 'rnace-50c31.firebaseapp.com',
+        projectId: 'rnace-50c31',
+        storageBucket: 'rnace-50c31.appspot.com',
+        messagingSenderId: '626137220500',
+        appId: '1:626137220500:web:33bfa04ed535711ec0bb81'
+    },
+    firebaseVapidKey: 'BMalm-lN0WqkFf6MPpQVb0N3gg8D5xSGLVd7XMBBrydXZkGiz2xTPxiQsR31x4nJj4cio_Io6XUJR43sz2ECy7g'
+};
